cd9dbacf5f4b0613601d9690a523c6a6b45f619d
yursds <yursds@outlook.com>
2024-03-01 10:25:20 +0100

    ilc update

--- controllerILC.py
+++ controllerILC.py
@@ -1,19 +1,22 @@
 import torch
-from tensordict     import TensorDict, TensorDictBase
+from tensordict     import TensorDict
 
-SCALE_Q = 0.1
-SCALE_L = 0.1
+SCALE_Q = 1
+SCALE_L = 1
 
 class controllerILC():
-    """ This class implements a model free ILC action. """
+    """ This class implements a model free ILC action ONLY for square MIMO. """
     
-    def __init__(self, dim_u:torch.int32, dim_e:torch.int32, Q=None, L=None):
+    def __init__(self, dim_u:int, dim_e:int, Q=None, L=None):
         """ Args:
-                dim_u (int16): dimension of input vector (single step).
-                dim_e (int16): dimention of error vector (single step).
-                dt (float16): sampling time (single step).
+                dim_u (int): dimension of input vector (single step).
+                dim_e (int): dimention of error vector (single step).
                 Q (torch.Tensor[optional]): Q-filter matrix (square matrix)
                 L (torch.Tensor[optional]): learning gain matrix (square matrix) """
+
+        # check        
+        if dim_u != dim_e:
+            raise TypeError("ILC class implemented only for square MIMO.")
         
         # init Q and L
         if Q is not None:
@@ -35,8 +38,8 @@ class controllerILC():
         else:
             self.L:torch.Tensor = torch.tril(torch.ones(dim_e, dim_e))*SCALE_L
             
-        self.u:torch.Tensor     = torch.Tensor()   # last control input
-        self.e:torch.Tensor     = torch.Tensor()   # last error
+        self.u:torch.Tensor     = torch.zeros([dim_u,1])   # last control input
+        self.e:torch.Tensor     = torch.zeros([dim_e,1])   # last error
         self.flagNewEp:bool     = True             # flag for new episode
         self.ep:int             = 0
         self.idx:int            = 0
@@ -45,42 +48,49 @@ class controllerILC():
         # data memory template (of error and input) 
         self.__tmplMem:TensorDict = TensorDict(
             {
-            'error': torch.Tensor(self.e), 
-            'input': torch.Tensor(self.u)
+            'error': torch.Tensor(), 
+            'input' : torch.Tensor()
             },
             batch_size=[]
         )
         
-        self.K_P = 0.1 * torch.eye(dim_u, dim_u)    #
-        self.K_V = 0.01 * torch.eye(dim_u, dim_u)   #
-        self.K_A = 0.001 * torch.eye(dim_u, dim_u)  #
-    
-    def updateMemory(self, e_new:torch.Tensor, u_new:torch.Tensor) -> None:
+    def __updateMem__(self, data:torch.Tensor, dict:str) -> None:
         """
-        Store new error and input a list of tensordict.
-        For new episode, create new tensordict to store new data.
+        Store new data in a tensordict in a list.
+        For new episode append new tensordict.
         """
         if self.flagNewEp:
             self.flagNewEp = False
-            self.mem.append(self.__tmplMem)
-            tmp_memE = e_new
-            tmp_memU = u_new
-        else:    
-            # use newest tensordict
-            tmp_memE:torch.Tensor = self.mem[-1]['error']        
-            tmp_memU:torch.Tensor = self.mem[-1]['input']
-            tmp_memE = torch.cat(tmp_memE,e_new,dim=1)      # stack respect row
-            tmp_memU = torch.cat(tmp_memU,u_new,dim=1)
-            
-        self.mem[-1]['error'] = tmp_memE
-        self.mem[-1]['input'] = tmp_memU
-                
-    def resetEp(self) -> None:
+           
+        # use newest tensordict
+        tmp_mem:torch.Tensor = self.mem[-1][dict]
+        tmp_mem = torch.cat([tmp_mem.clone(),data],dim=1)      # stack respect row
+        
+        self.mem[-1][dict] = tmp_mem
+           
+    def updateMemError(self, e_new:torch.Tensor) -> None:
+        """
+        Store new error in a tensordict in a list.
+        For new episode append new tensordict.
+        """
+        dict:str='error'
+        self.__updateMem__(e_new, dict)
+    
+    def updateMemInput(self, u_new:torch.Tensor) -> None:
+        """
+        Store new input in a tensordict in a list.
+        For new episode append new tensordict.
+        """
+        dict:str='input'
+        self.__updateMem__(u_new, dict)
+   
+    def newEp(self) -> None:
         """ 
         Reset flag for new episode.
         Reset step index. 
         """
         self.flagNewEp = True
+        self.mem.append(self.__tmplMem.clone())
         self.idx = 0
         self.ep += 1
         
@@ -101,19 +111,19 @@ class controllerILC():
         Single step of ILC.
         Update control.
         """
-        k:int               = self.idx
+        k   = self.idx
+        Q   = self.Q
+        L   = self.L
         
         if self.ep == 0:
-            u_old:torch.Tensor  = self.mem[-2]["input"][:,k]
-            e_old:torch.Tensor  = self.mem[-2]["error"][:,k]
+            print("banana")
+            u_old:torch.Tensor  = self.mem[-2]["input"][:,k:k+1]
+            e_old:torch.Tensor  = self.mem[-2]["error"][:,k:k+1]
         else:
-            u_old:torch.Tensor  = self.mem[-2]["input"][:,k]
-            e_old:torch.Tensor  = self.mem[-2]["error"][:,k]
+            u_old:torch.Tensor  = self.mem[-2]["input"][:,k:k+1]
+            e_old:torch.Tensor  = self.mem[-2]["error"][:,k:k+1]
             
-        Q:torch.Tensor      = self.Q
-        L:torch.Tensor      = self.L
-        
-        u_new:torch.Tensor = Q*(u_old+L*e_old)
+        u_new:torch.Tensor = torch.mm(Q,(u_old+torch.mm(L,e_old)))
         self.u = u_new
         
         self.idx += 1 
@@ -125,29 +135,76 @@ class controllerILC():
         self.flagNewEp = True
         self.idx = 0
         self.mem = []
+
+    def firstEpLazy(self, ref:torch.Tensor) -> None:
+        """ no input and save error as reference"""
+        self.updateMemError(ref)
+        self.updateMemInput(torch.zeros(self.u.size()))
         
+                
         
 if __name__ == '__main__':
     
-    a = controllerILC(2,2)
-    ee = torch.tensor([2,1]).view(-1,1)
-    #uu = torch.tensor([3,5]).view(-1,1)
+    # definition of reference
+    ref = torch.tensor([[i,i,i] for i in range(10)]).t()
     
-    for _ in range(10):
-        a.step()
-        uN = a.getControl()
-        a.updateMemory(ee,uN)
+    conILC = controllerILC(3,3)     # ILC controller instance
+    conILC.newEp()                  # start new episode
+
+    # initialization of ILC memory
+    # controller do nothing and save error as reference
+    for k in range(ref.shape[1]):
+        conILC.firstEpLazy(ref[:, k:k+1])
         
-    a.resetEp()
-    for _ in range(5):
-        a.step()
-        uN = a.getControl()
-        a.updateMemory(ee,uN)
+    a = conILC.getMemory()                      # get memory
+    
+    # start ILC iteration
+    for k in range(50):
         
-    b = a.getMemory()
-    print("my memory", b)
-    a.resetAll()
-    b = a.getMemory()
+        conILC.newEp()                          # start new episode
+        
+        for k in range(ref.shape[1]):
+            
+            conILC.step()
+            new_u = conILC.getControl()
+            conILC.updateMemInput(new_u)        # save new_control
+            out = new_u/2                       # simulate robot (simple function)
+            new_e = ref[:, k:k+1]-out           # get new error
+            conILC.updateMemError(new_e)        # save new_error
+            
+        b = conILC.getMemory()
+    
+    
+    from matplotlib import pyplot as plt
+
+    last_err = []
+
+    # Ciclo su ciascun TensorDict in b
+    for tensor_dict in b:
+        
+        # Estrai il tensore associato a "error"
+        errore_tensor = tensor_dict["error"]
+        
+        # Estrai l'ultima colonna del tensore
+        ultima_colonna = errore_tensor[:, -1]
+        
+        # Aggiungi l'ultima colonna alla lista delle ultime colonne
+        last_err.append(ultima_colonna)
+    
+    plt.ion()
+
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(last_err)
+    plt.title("error")
+    plt.xlabel("iteration")
+
+    print('Complete')
+    plt.ioff()
+    plt.show()
+    
+    conILC.resetAll()
+    b = conILC.getMemory()
     print("my memory lost", b)
-    
     print("finish")
